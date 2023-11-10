c209c3ddbae7c99b1f9acb38b233d08e0022d193
Sarfrazafzal <143732552+Sarfrazafzal@users.noreply.github.com>
2023-11-10 20:25:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCR9d7Sj1PzOcX2q3-ZwdxNDYcpvXD8x90",
+  authDomain: "login-94725.firebaseapp.com",
+  projectId: "login-94725",
+  storageBucket: "login-94725.appspot.com",
+  messagingSenderId: "930227497126",
+  appId: "1:930227497126:web:66f8e6144ce7bd290eec67",
+  measurementId: "G-HT9KV3HQR2"
+};
+
+var app = firebase.initializeApp(firebaseConfig);
+
+function result(){
+    var email = document.getElementById("email")
+    var password = document.getElementById("password")
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage)
+    // ..
+  });
+}
+
+function getValues(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user)
+    // IdP data available in result.additionalUserInfo.profile.
+      // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage)
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+getValues()
+
+function github(){
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase
+  .auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    var token = credential.accessToken;
+
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user)
+    // IdP data available in result.additionalUserInfo.profile.
+      // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage)
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+github()
